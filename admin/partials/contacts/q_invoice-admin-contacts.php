d541fde0bf152e046b5f1dcdee2855e7b8a72157
TokenV23 <79996143+TokenV23@users.noreply.github.com>
2022-03-07 14:19:23 +0100

    contacts pupup design fix

--- admin/partials/contacts/q_invoice-admin-contacts.php
+++ admin/partials/contacts/q_invoice-admin-contacts.php
@@ -149,7 +149,7 @@
         </table>
     </div>
 
-    <div id="contactOverlay" class="overlay" style="display:none">
+    <div id="contactOverlay" class="overlay" style="display:none; left: 0px;">
         <div id="qiContactPopup">
             <h2><?php echo __("New Contact", "ev")?></h2>
             <form 
